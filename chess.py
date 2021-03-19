6e5db31f5212f4af599eee83c54d45fad7598137
vwallajabad <76623910+vwallajabad@users.noreply.github.com>
2021-03-19 16:03:33 -0500

    Create chess.py

---
+++ chess.py
@@ -0,0 +1,34 @@
+import chess
+
+#creates the board
+board = chess.Board()
+
+#shows chess board
+print(board)
+
+repeat = 1
+while repeat < 6:
+	board.legal_moves
+	#White move
+	Whitemove = input("Enter your move in notation form: ")
+	board.push_san(Whitemove)
+	print(board)
+
+	Blackmove = input("Enter your move: ")
+	board.push_san(Blackmove)
+	print(board)
+
+	#ENDGAMES
+	#checks for check
+	check = board.is_check()
+	checkmate = board.is_checkmate()
+	draw = board.is_stalemate()
+
+	if check:
+		print("You are in check!")
+	if checkmate:
+		print("your opponent checkmated you...")
+		break
+	elif draw:
+		print("Everyone won it is a stalemate...")
+		break
